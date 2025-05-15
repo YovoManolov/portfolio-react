f438b9cbd2dcf7b9d18160e9eedeff26c20f3a62
Yovo Manolov <yovo.manolov@abv.bg>
2025-05-16 00:58:51 +0300

    Update education and experience details; modify skills list to reflect current expertise

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,14 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Master's Degree, Computer and Software Engineering",
+    duration: "2020 - 2022",
+    institution: "Technical University of Sofia",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
-  },
-  {
-    id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "Bachelor's Degree, Computer and Software Engineering",
+    duration: "2015 - 2020",
+    institution: "Technical University of Sofia",
   }
 ]
--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,38 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: 'Technology Lead',
+    company: "Infosys",
+    duration: "(Jan 2025 - Present)"
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Full-Stack Engineer",
+    company: "TechPods",
+    duration: "(Jul 2024 - Oct 2024)"
   },
   {
     id: 3,
-    title: "Self Employed",
-    company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
+    title: "Senior Java Developer",
+    company: "GAN",
+    duration: "(Jul 2023 - Mar 2024)"
+  },
+  {
+    id: 4,
+    title: "Abiliton Intermediate Software Engineer",
+    company: "SoftServe",
+    duration: "(Aug 2022 - Jul 2023)"
+  },
+  {
+    id: 5,
+    title: "Software Engineer",
+    company: "TIS",
+    duration: "(Jan 2021 - Aug 2022)"
+  },
+  {
+    id: 6,
+    title: "Software Engineer",
+    company: "Raiffeisenbank Bulgaria",
+    duration: "(Oct 2017 - Apr 2021)"
   }
 ]
