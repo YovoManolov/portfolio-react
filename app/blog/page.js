4e1b69d45cda0eec061d92a74f9899436d021d41
Yovo Manolov <yovo.manolov@abv.bg>
2025-05-31 20:42:30 +0300

    Enhance blog data fetching by adding console log for fetched blogs and updating cover image fallback logic

--- app/blog/page.js
+++ app/blog/page.js
@@ -29,11 +29,11 @@ async function getBlogs() {
 
 async function page() {
   const blogs = await getBlogs();
-
+  console.log("Blogs fetched:", blogs);
   return (
     <div className="py-8">
       <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
+        <div className="flex items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
             All Blogs
@@ -45,8 +45,7 @@ async function page() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
         {
           blogs.map((blog, i) => (
-            blog?.cover_image &&
-            <BlogCard blog={blog} key={i} />
+            <BlogCard blog={{ ...blog, cover_image: blog.cover_image || blog.social_image }} key={i} />
           ))
         }
       </div>
